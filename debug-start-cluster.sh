0179f1975a9cc01846cb22aae63ad19a7b0939e2
Josh Bowen <jskybowen@gmail.com>
2022-02-15 11:16:57 -0700

    debug stuff

--- debug-start-cluster.sh
+++ debug-start-cluster.sh
@@ -5,20 +5,20 @@ chmod 0600 celestia-node/full/*/nodekey*
 chmod 0600 celestia-node/light/*/nodekey*
 chmod 0600 dalc/celestia-light/keys/*
 
-# Start core nodes
-docker-compose -f docker/core-docker-compose.yml up -d 
+# Start core0 core node
+docker-compose -f docker/core-docker-compose.yml up core0 core1 core2  -d 
 
-echo "Waiting 40s for core nodes to produce a block"
-sleep 40s
-
-# Start bridge nodes
-docker-compose -f docker/bridge-docker-compose.yml up -d
-
-# echo "Waiting 30s for bridge nodes to sync a block"
+echo "Waiting 30s for core nodes to produce a block"
 sleep 30s
 
-# Start light nodes
-docker-compose -f docker/light-docker-compose.yml up -d
+# Start full0 bridge node
+docker-compose -f docker/debug-bridge.yml up -d
+
+echo "Waiting 10s for bridge nodes to sync a block"
+sleep 10s
+
+# Start light0 light node
+docker-compose -f docker/debug-light.yml up light0 -d
 
 echo "Waiting 10s for light nodes to perform DA sampling"
 sleep 10s
@@ -33,9 +33,7 @@ echo "Sleeping 10s to wait for DALC funding tx to go through"
 sleep 10s
 
 # Start the evmos node
-# docker-compose -f docker/evmos-docker-compose-debug.yml up -d
 docker-compose -f docker/debug-evmos.yml up -d
-
 
 # echo "Sleeping 10s to wait for evmos block"
 # sleep 10s
