0f574813430d14db81b44404807ffae606242d5a
Josh Bowen <josh@celestia.org>
2022-04-29 12:42:13 +0000

    wip

--- scripts/mock-ethermint.sh
+++ scripts/mock-ethermint.sh
@@ -6,11 +6,11 @@ docker compose -f docker/mock-dalc-docker-compose.yml up -d
 
 # Start the regular ethermint node
 echo "Creating Ethermint0 node(s)"
-docker compose -f docker/ethermint-docker-compose.yml up ethermint0 -d
+docker compose -f docker/ethermint-mock-docker-compose.yml up ethermint0 -d
 
 echo sleep 5
 sleep 5
 
 # Start the regular ethermint node
 echo "Creating Ethermint1 node(s)"
-docker compose -f docker/ethermint-docker-compose.yml up ethermint1 -d
+docker compose -f docker/ethermint-mock-docker-compose.yml up ethermint1 -d
