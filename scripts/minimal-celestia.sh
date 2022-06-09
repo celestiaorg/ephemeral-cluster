45d651ed00b95c704a39bbe7d50c30fb470ca69c
Josh Bowen <jskybowen@gmail.com>
2022-06-09 17:38:27 +0200

    wip

--- scripts/minimal-celestia.sh
+++ scripts/minimal-celestia.sh
@@ -9,8 +9,8 @@ chmod 0600 dalc/celestia-light/keys/*
 echo "Creating core node(s)"
 docker compose -f docker/minimal/core-docker-compose.yml up -d 
 
-echo "Waiting 30s for core nodes to produce a block"
-sleep 30s
+echo "Waiting 15s for core nodes to produce a block"
+sleep 15s
 
 # Start bridge0 bridge node
 echo "Creating bridge node(s)"
