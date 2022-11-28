fdc11113d8cecc5bbaab000d9b8badcfa2323aa5
Douglas Chimento <dchimento@gmail.com>
2022-11-28 22:18:49 +0200

    * fix location of priv_validator_key * upgrade to celestia-app:v0.10.0-rc1 * remove uri* from genesis

--- scripts/teardown-docker-cluster.sh
+++ scripts/teardown-docker-cluster.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-docker kill $(docker ps -q)
-docker rm $(docker ps -a -q)
+for name in core0 dalc0 light0 ethermint0; do
+    docker_id=$(docker ps -q  -a --filter name=$name)
+    [ -n "$docker_id" ] && (docker kill $docker_id; docker rm $docker_id)
+done
