831631d5f63405a2850303fdcceaaca7dfb113e3
Jordan Oroshiba <jordan.oroshiba@gmail.com>
2023-01-04 11:31:15 -0800

    Fixing teardown script

--- scripts/teardown-docker-cluster.sh
+++ scripts/teardown-docker-cluster.sh
@@ -2,7 +2,7 @@
 
 
 # Only deleting docker containers which our scripts might actually build
-CONTAINERS=$(docker ps -a -q --filter "name=bridge0" --filter "name=light0" --filter "name=core0" --filter "name==ethermint0")
+CONTAINERS=$(docker ps -a -q --filter "name=bridge0" --filter "name=light0" --filter "name=core0" --filter "name=ethermint0")
 if [ ! -z "$CONTAINERS" ]
 then
     echo "Killing currently running ephemeral cluster containers"
